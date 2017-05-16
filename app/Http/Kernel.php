e4e833e8c8c67f1e875fa91a60727891a1d3681f
Sebastian Noske <sebastian.noske@ftwk.de>
2017-05-16 14:17:42 +0200

    fixed prefligth issue

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,8 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \Barryvdh\Cors\HandleCors::class,
-        \Barryvdh\Cors\HandlePreflightSimple::class
+        \Barryvdh\Cors\HandleCors::class
     ];
 
     /**
