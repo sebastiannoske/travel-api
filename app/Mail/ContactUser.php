234bfee01d8cdbc066dbeb06d2e689c4f75b9788
basti <sebastian.noske@gmail.com>
2018-05-25 13:17:45 +0200

    add sendmail api route

--- app/Mail/ContactUser.php
+++ app/Mail/ContactUser.php
@@ -15,7 +15,7 @@ class ContactUser extends Mailable
 
     public $user;
     public $fromMail;
-    public $message;
+    public $fromMessage;
 
     /**
      * Create a new message instance.
@@ -26,7 +26,7 @@ class ContactUser extends Mailable
     {
         $this->user = $user;
         $this->fromMail = $request->email;
-        $this->message = $request->email; // $request->description;
+        $this->fromMessage = $request->email; // $request->description;
     }
 
     /**
