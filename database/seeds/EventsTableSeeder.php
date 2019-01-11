4a16af848592eb00e7568f3117934b17ef421e63
basti <sebastian.noske@gmail.com>
2019-01-11 08:51:05 +0100

    sme seeding

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -14,8 +14,8 @@ class EventsTableSeeder extends Seeder
     {
 
         DB::table('events')->insert([
-            'name' => 'Klimastreik',
-            'campaignText' => 'Mitfahrbörse zum Klimastreik 2019<br/><br/>25.01.2019 - Bundeswirtschaftsministerium - Berlin'
+            'name' => 'Bildungsstreik fürs Klima',
+            'campaignText' => 'Mitfahrbörse zum Bildungsstreik fürs Klima 2019<br/><br/>25.01.2019 - Bundeswirtschaftsministerium - Berlin'
         ]);
 
         /* DB::table('events')->insert([
